ff456daf354e22956945ab3fbf0b9204473d273a
Alex Marius H. <hritcualex862@gmail.com>
2026-07-22 23:28:25 +0300

    Other

--- Server/src/Main.kt
+++ Server/src/Main.kt
@@ -1,13 +1,11 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import com.CliChannel
+import com.CommandDispatcher
+import dal.DatabaseExecutor
+import dal.DatabaseFacade
+import dal.SQLiteConnection
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
+fun main() {
+    val dispatcher = CommandDispatcher(DatabaseFacade(DatabaseExecutor(SQLiteConnection("test"))))
+    val cli = CliChannel(dispatcher)
+    cli.start()
 }
