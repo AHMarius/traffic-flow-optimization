75a8965e0670765aafbfe61c6b5812cddf97a202
rares <rares-constantin.andronic@student.tuiasi.ro>
2026-08-15 22:40:50 +0300

    mid save

--- Client/DAL/facade.py
+++
@@ -1,6 +0,0 @@
-class DataBaseFacade:
-    def __init__(self, repository, dispatcher):
-        self.__repository = repository
-        self.__dispatcher = dispatcher
-        self.__pending = map()
-    def submit(self, request, priority):
